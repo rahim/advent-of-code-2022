476e26da1b1f70c7aa9e25519f8bec2fd4b75f99
Rahim Packir Saibo <i.am@rah.im>
2022-12-02 14:32:55 +0000

    rustfmt

--- 2022-12-01/calorie-counting/src/main.rs
+++ 2022-12-01/calorie-counting/src/main.rs
@@ -2,14 +2,18 @@ fn main() {
     println!("Hello, world!");
 }
 
-
 pub fn most_calories_for_an_elf(input: &str) -> u32 {
     let list_for_each_elf: Vec<&str> = input.split("\n\n").collect();
-    let calorie_collection_for_each_elf: Vec<Vec<u32>> =
-      list_for_each_elf.iter().map(|&s| s.lines().map(|x: &str| x.parse::<u32>().unwrap() ).collect()).collect();
+    let calorie_collection_for_each_elf: Vec<Vec<u32>> = list_for_each_elf
+        .iter()
+        .map(|&s| s.lines().map(|x: &str| x.parse::<u32>().unwrap()).collect())
+        .collect();
     println!("{:#?}", calorie_collection_for_each_elf);
 
-    let totals_for_each_elf: Vec<u32> = calorie_collection_for_each_elf.iter().map(|c| c.iter().sum::<u32>()).collect();
+    let totals_for_each_elf: Vec<u32> = calorie_collection_for_each_elf
+        .iter()
+        .map(|c| c.iter().sum::<u32>())
+        .collect();
     println!("{:#?}", totals_for_each_elf);
 
     return *totals_for_each_elf.iter().max().unwrap();
@@ -21,8 +25,7 @@ mod tests {
 
     #[test]
     fn test_most_calories_for_an_elf() {
-        let input =
-"1000
+        let input = "1000
 2000
 3000
 
