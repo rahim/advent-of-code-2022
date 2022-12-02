38eb36e2e3f35a4c5f2eb09d73883ee6c7023216
Rahim Packir Saibo <i.am@rah.im>
2022-12-02 10:27:17 +0000

    Generate rust project structure

---
+++ 2022-12-01/calorie-counting/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
