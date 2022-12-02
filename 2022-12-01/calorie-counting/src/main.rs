094718caaa9ef1df2859ca3841be2c92f5c854ef
Rahim Packir Saibo <i.am@rah.im>
2022-12-02 13:32:24 +0000

    Add failing test

--- 2022-12-01/calorie-counting/src/main.rs
+++ 2022-12-01/calorie-counting/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     println!("Hello, world!");
 }
 
+
+fn most_calories_for_an_elf(input: &str) -> usize {
+    return 0;
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -14,5 +19,27 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+
+    #[test]
+    fn test_most_calories_for_an_elf() {
+        let input =
+"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+        let result = most_calories_for_an_elf(input);
+        assert_eq!(result, 2400)
     }
 }
