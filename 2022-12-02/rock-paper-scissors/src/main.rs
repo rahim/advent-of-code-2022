cc9f5d84430a2730831eaf6a4cbc59625de4adc7
Rahim Packir Saibo <i.am@rah.im>
2022-12-03 18:31:12 +0000

    Fleshing day 2

--- 2022-12-02/rock-paper-scissors/src/main.rs
+++ 2022-12-02/rock-paper-scissors/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 fn main() {
     let path = "strategy-guide.txt";
     let contents = fs::read_to_string(path).expect("Failed to read file");
-    
+
     println!("Expected guide score: {}", expected_guide_score(&contents));
 }
 
@@ -10,11 +10,40 @@ pub fn expected_guide_score(input: &str) -> u32 {
     return 0;
 }
 
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+struct Round {
+    p1: Play,
+    p2: Play
+}
+
+impl Round {
+    fn score(&self) -> u8 {
+        0
+    }
+
+    fn shape_score(&self) -> u8 {
+        match self.p1 {
+            Play::Rock => 1,
+            Play::Paper => 2,
+            Play::Scissors => 3,
+        }
+    }
+
+    fn outcome_score(&self) -> u8 {
+        0
+    }
+} 
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use indoc::indoc;
-    
+
     const EXAMPLE_INPUT: &str = indoc! {"
         A Y
         B X
@@ -25,5 +54,19 @@ mod tests {
     fn test_expected_guide_score() {
         let result = expected_guide_score(EXAMPLE_INPUT);
         assert_eq!(result, 15)
+    }
+
+    #[test]
+    fn test_shape_score() {
+        assert_eq!(Round{p1: Play::Rock,     p2: Play::Paper}.shape_score(), 1);
+        assert_eq!(Round{p1: Play::Paper,    p2: Play::Paper}.shape_score(), 2);
+        assert_eq!(Round{p1: Play::Scissors, p2: Play::Paper}.shape_score(), 3);
+    }
+
+    #[test]
+    fn test_outcome_score() {
+        assert_eq!(Round{p1: Play::Rock,     p2: Play::Paper}.outcome_score(), 0);
+        assert_eq!(Round{p1: Play::Paper,    p2: Play::Paper}.outcome_score(), 3);
+        assert_eq!(Round{p1: Play::Scissors, p2: Play::Paper}.outcome_score(), 6);
     }
 }
